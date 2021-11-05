7a5b15617c0d529cb65c87a95a722dbd7f405c6a
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-05 06:56:58 -0300

    refactor: :recycle: refactor createTask and findAllTasks

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -6,25 +6,21 @@ const taskExists = async (note) => {
   return task !== null;
 };
 
-const createTask = async ({ note, status }) => {
-  const task = await connection.getConnection()
-    .then((db) => db.collection('tasks')
-      .insertOne({
-        note, status, createdAt: new Date(Date.now()), updatedAt: new Date(Date.now()),
-      }))
-    .then(({ insertedId, ops: [result] }) => ({
-      id: insertedId,
-      note: result.note,
-      status: result.status,
-      createdAt: result.createdAt,
-      updatedAt: result.updatedAt,
-    }));
-
-  return task;
-};
+const createTask = async ({ note, status }) => connection.getConnection()
+  .then((db) => db.collection('tasks')
+    .insertOne({
+      note, status, createdAt: new Date(Date.now()), updatedAt: new Date(Date.now()),
+    }))
+  .then(({ insertedId, ops: [result] }) => ({
+    id: insertedId,
+    note: result.note,
+    status: result.status,
+    createdAt: result.createdAt,
+    updatedAt: result.updatedAt,
+  }));
 
 const findAllTasks = async () => connection.getConnection()
-  .then((db) => db.collection('tasks').find({}).toArray());
+  .then((db) => db.collection('tasks').find().toArray());
 
 module.exports = {
   createTask,
