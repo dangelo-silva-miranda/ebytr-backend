687def7f0fe161a705ed3c2f07606f3edeb36b4a
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-05 02:21:22 -0300

    refactor: :recycle: change the task description field

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,14 +1,14 @@
 const connection = require('../database/connection');
 
-const createTask = ({ task, status }) => {
+const createTask = ({ note, status }) => {
   connection.getConnection()
     .then((db) => db.collection('tasks')
       .insertOne({
-        task, status, createdAt: new Date(Date.now()), updatedAt: new Date(Date.now()),
+        note, status, createdAt: new Date(Date.now()), updatedAt: new Date(Date.now()),
       }))
     .then(({ insertedId, ops: [result] }) => ({
       id: insertedId,
-      task: result.task,
+      note: result.note,
       status: result.status,
       createdAt: result.createdAt,
       updatedAt: result.updatedAt,
