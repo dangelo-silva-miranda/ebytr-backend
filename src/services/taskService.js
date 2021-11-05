087e49d0c9229503f1f7a416a0bca8434f40c4fe
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-05 06:04:46 -0300

    feat: :sparkles: check task fields using taskDataSchema and Joi

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,17 +1,24 @@
 const { StatusCodes } = require('http-status-codes');
 const taskModel = require('../models/taskModel');
+const { taskDataSchema } = require('./schemas');
 
 const titleCase = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
-const createTask = async ({ note: newNote, status }) => {
-  const note = titleCase(newNote.trim());
+const createTask = async ({ note, status }) => {
+  const { error } = taskDataSchema.validate({ note, status });
+  if (error) { // error.isJoi indentifica se o erro foi do tipo Joi
+    const { message } = error.details[0];
+    return { code: StatusCodes.BAD_REQUEST, message };
+  }
+
+  const newNote = titleCase(note.trim());
 
   const taskExists = await taskModel.taskExists(note);
   if (taskExists) {
     return { code: StatusCodes.CONFLICT, message: 'Task already registered.' };
   }
 
-  const task = await taskModel.createTask({ note, status });
+  const task = await taskModel.createTask({ note: newNote, status });
 
   return {
     code: StatusCodes.CREATED,
