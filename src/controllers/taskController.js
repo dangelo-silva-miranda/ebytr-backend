5250a5c99ef952fa1878e3a71dd4695b03aa0323
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-05 09:56:44 -0300

    feat: :sparkles: implement updateTaskById in taskController

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -15,7 +15,19 @@ const findAllTasks = async (_req, res) => {
   return res.status(code).json(tasks);
 };
 
+const updateTaskById = async (req, res) => {
+  const { params: { id }, body: { note, status } } = req;
+  const { code, message, task } = await taskService.updateTaskById({ id, note, status });
+
+  if (!task) {
+    return res.status(code).json({ message });
+  }
+
+  return res.status(code).json(task);
+};
+
 module.exports = {
   createTask,
   findAllTasks,
+  updateTaskById,
 };
